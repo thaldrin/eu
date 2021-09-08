dfd8e2251e5c0ad6a6f1743193786285e55e4ce3
Lio Young <lio@werewolf.design>
2021-09-08 11:40:41 +0200

    basic typings

--- src/misc/types.ts
+++ src/misc/types.ts
@@ -1,70 +1,89 @@
 import { Client as DiscordClient, Collection } from "./imports";
 import { Client as RevoltClient } from "revolt.js";
-//  SlashCommand = {
-//     // Command Specific
-//     name: string
-//     description: string
-//     permissions?: string | string[]
-//     global?: boolean
-//     guilds?: string | string[]
-//     nsfw?: boolean
-
-//     guild?: boolean
-//     dev?: boolean
-//     usage?: string
-
-// }
-
-// type Construct = {
-//     events: string
-//     commands: string
-//     token: string
-//     options: {
-//         intents: any,
-//     }
-//     // config: Config
-// }
 
 declare namespace Eu {
+    /**
+     * The Main Client, universal between both Discord and Revolt
+     */
     interface EuClient {
+        /** 
+         * Collection of Commands using Discord.JS Collection functionality
+         */
         commands: Collection<string, Command>
+        /** 
+         * Collection of Events using Discord.JS Collection functionality
+         */
         events: Collection<string, Event>
+        /** 
+         * Collection of Cooldowns using Discord.JS Collection functionality
+         */
         cooldowns: Collection<unknown, unknown>
-
-        // config?: Config
-        eval: any
+        /** The Config, provided by the user and used throughout the Framework */
         construct: Construct
     }
-
+    /** The Config, provided by the user and used throughout the Framework */
     interface Construct {
+        /** The Discord Client */
         discord?: DiscordClient
+        /** The Revolt Client */
         revolt?: RevoltClient
+        /** the path where the event files are stored */
         events: string
+        /** the path where the commands files are stored */
         commands: string
+        /** the bot token */
         token: string
         options?: {
+            /** [DISCORD]
+             * Intents the Bot needs to work on Discord
+             */
             intents?: any,
+            /** [REVOLT]
+             * The Revolt API URL, if the but runs on a Self-Hosted Revolt Instance
+             */
             apiURL?: string,
+            /** 
+             * Devs of the Bot, used for the Developer Commands
+            */
+            devs?: string | string[],
+            /** 
+             * If you want Eu to automatically set the Categories or if you want to do it manually
+            */
+            commandCategories?: boolean
         }
     }
     interface Command {
-        // Command Specific
+        /** The Command name, primary way of calling the command
+         * @exmaple `!ping`
+         */
         name?: string
+        /** The Command description, used for the help command */
         description?: string
+        /** How long the command is unusable, usually for a single User */
         cooldown?: number
+        /** The Permissinons needed for a Command to run */
         permissions?: string | string[]
+        /** If the Command shows up in the Help Command */
         hidden?: boolean
+        /** If the Command is only allowed to be ran in NSFW Channels */
         nsfw?: boolean
+        /** The Category the command is in */
+        category?: string
 
+        /** If the Command is only allowed to be ran in guilds */
         guild?: boolean
+        /** If the Command is only allowed to be ran by Developers */
         dev?: boolean
+        /** How to use the command */
         usage?: string
+        /** Other "Names" for the Command */
         aliases?: string[]
 
 
     }
 
     interface Event {
+        /** Event name, requires whatever client.on() can listen to */
         name: string
     }
 }
